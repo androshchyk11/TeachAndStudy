348d707cb6ba4b1ffe599f05e73925acfee6f30e
artemmar <artemmarcoop@gmail.com>
2021-03-13 14:58:06 +0200

    base adapter added, new features added

--- app/src/main/java/com/example/ts/view/activities/SplashActivity.kt
+++ app/src/main/java/com/example/ts/view/activities/SplashActivity.kt
@@ -1,14 +1,20 @@
 package com.example.ts.view.activities
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
+import androidx.core.animation.doOnEnd
+import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.LottieDrawable
 import com.example.ts.R
 import com.example.ts.managers.SharedPreferencesManager
 import com.example.ts.view.activities.abstraction.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.activity_splash.*
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class SplashActivity : BaseActivity() {
@@ -25,19 +31,28 @@ class SplashActivity : BaseActivity() {
     }
 
     private fun checkIfUserIsAuthorized() {
-        Observable.empty<Any>()
-            .delay(2000L, java.util.concurrent.TimeUnit.MILLISECONDS)
-            .subscribeOn(AndroidSchedulers.mainThread())
-            .doOnComplete {
-                if (!this@SplashActivity.isFinishing) {
-                    finish()
+        val animator = ValueAnimator.ofFloat(0f, 0.1f)
+        animator.addUpdateListener {
+            animationView.progress = animator.animatedValue as Float
+        }
+        animator.start()
+        animator.doOnEnd {
+            Observable.empty<Any>()
+                .delay(2000L, java.util.concurrent.TimeUnit.MILLISECONDS)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .doOnComplete {
+                    if (!this@SplashActivity.isFinishing) {
+                        finish()
 //                    if (sharedPreferencesManager.userAccessToken.isNullOrEmpty()) {
-//                        startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
+                        startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
 //                    } else {
-                        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+//                        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
 //                    }
+                    }
                 }
-            }
-            .subscribe()
+                .subscribe()
+        }
+//        animation.comp
+
     }
 }
